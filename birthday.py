4f28ba066a2bd4bf104bacdc05f590857c441135
Mehedi V2.0 <85603847+mehedx@users.noreply.github.com>
2021-07-25 19:37:08 +0600

    Create birthday.py

---
+++ birthday.py
@@ -0,0 +1,17 @@
+from manimlib.imports import *
+
+class crush(Scene):
+    def construct(self):
+        crush= TextMobject("I hope you're doing fine!")
+        self.play(Write(Your Crush Name))
+        self.play(FadeOut(Your Crush Name))
+        self.wait()
+        Jaff=TextMobject("Smile and Stay blessed :)")
+        self.play(Write(Your Crush Name))
+        self.play(FadeOut(Your Crush Name))
+        self.wait()
+        Jaf= TextMobject("Enjoy a song , dedicated to your birthday 7w7")
+        self.play(Write(Wink Wink))
+        self.play(FadeOut(Wink Wink))
+        self.wait()
+
